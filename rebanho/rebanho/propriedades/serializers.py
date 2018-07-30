fb17a682c2aaed64dcde7cd43365c031fcdd9d3d
Fabiano Almeida <fabiano@znc.com.br>
2018-07-29 21:52:27 -0300

    ref #14 cria o serializers e faz os 1º testes importando no terminal

---
+++ rebanho/rebanho/propriedades/serializers.py
@@ -0,0 +1,10 @@
+from rebanho.propriedades.models import AnimalPesagem
+from rest_framework import serializers
+
+
+class AnimalPesagemSerializer(serializers.ModelSerializer):
+  
+    class Meta:
+        model = AnimalPesagem
+        fields = ('id',  'animal', 'data', 'peso')
+
